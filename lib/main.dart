574e986cd00d1c5ecbead816a5fb8d65e8fd5f87
lirunkai <lirunkaigod@gmail.com>
2019-08-05 10:12:37 +0800

    done

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(
+  title: 'new Flutter world',
+  home: new Scaffold(
+    appBar: new AppBar(
+      actions: <Widget>[],
+      title: Text('just base home page'),
+    ),
+    body: new Center(
+      child: Text('hello world'),
+    ),
+  ),
+));
