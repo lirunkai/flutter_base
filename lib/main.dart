e0c3fb00005d9e0a4a5783ba22eb5d46289aa0e3
lirunkai <lirunkaigod@gmail.com>
2019-08-11 17:58:43 +0800

    done theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MaterialApp(
-  title: 'new Flutter world',
-  home: new Scaffold(
-    appBar: new AppBar(
-      actions: <Widget>[],
-      title: Text('just base home page'),
-    ),
-    body: new Center(
-      child: Text('hello world'),
-    ),
-  ),
-));
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    final appName = '自定义主题';
+    return new MaterialApp(
+      title: appName,
+      theme: new ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.lightGreen[600],
+        accentColor: Colors.orange[600]
+      ),
+      home: new MyHomePage(
+        title: appName
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  MyHomePage({Key key, @required this.title}):super(key:key);
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Container(
+          color: Theme.of(context).accentColor,
+          child: new Text(
+            '带有背景颜色的',
+            style: Theme.of(context).textTheme.title
+          ),
+        ),
+      ),
+      floatingActionButton: new Theme(
+        data: Theme.of(context).copyWith(accentColor: Colors.grey),
+        child: new FloatingActionButton(
+          onPressed: null,
+          child: new Icon(Icons.computer),
+        ),
+      ),
+    );
+  }
+}
