bd2e456163574e9591ebd0266c79e49b27eec4f1
lirunkai <lirunkaigod@gmail.com>
2019-08-13 07:24:04 +0800

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MaterialApp(
-  title: 'new Flutter world',
-  home: new Scaffold(
-    appBar: new AppBar(
-      actions: <Widget>[],
-      title: Text('just base home page'),
-    ),
-    body: new Center(
-      child: Text('hello world'),
-    ),
-  ),
-));
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Container',
+      home: Scaffold(
+        appBar: AppBar(
+          title: new Text('Container'),
+        ),
+        body: Center(
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              border: new Border.all(
+                color: Colors.green,
+                width: 8.0
+              ),
+              borderRadius: const BorderRadius.all(const Radius.circular(8.0))
+            ),
+            child: Text(
+              'Flutter',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
