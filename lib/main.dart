85dd331c7e39136237618be7d89e90f43bac4472
lirunkai <lirunkaigod@gmail.com>
2019-08-12 23:21:45 +0800

    url_launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(new MaterialApp(
-  title: 'new Flutter world',
-  home: new Scaffold(
-    appBar: new AppBar(
-      actions: <Widget>[],
-      title: Text('just base home page'),
-    ),
-    body: new Center(
-      child: Text('hello world'),
-    ),
-  ),
-));
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: '第三放url_launcher',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('使用url_launcher'),
+        ),
+        body: new Center(
+          child: new RaisedButton(
+            child: new Text('打开页面'),
+            onPressed: (){
+              const url = 'https://www.baidu.com';
+              launch(url);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
