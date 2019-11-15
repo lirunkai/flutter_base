d1e71f5bd753d54ed1b5e95b0f6993eaa57547ee
lirunkai <lirunkaigod@gmail.com>
2019-11-15 18:01:38 +0800

    hero

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_base/hero_page.dart';
+
+void main() => runApp(MyApp());
+
+double screenWidth;
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Some Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        'HeroPage': (context) => HeroPage()
+      }
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  MyHomePage({Key key, this.title}):super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Demo'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    navigateTo('HeroPage');
+                  },
+                  child: Text('HeroPage'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  navigateTo(name) {
+    Navigator.of(context).pushNamed(name);
+  }
+}
